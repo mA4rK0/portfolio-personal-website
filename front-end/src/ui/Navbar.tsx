7711b03b71f2c8c857a4ea6a67093b2399552649
mA4rK0 <onlymarko25@gmail.com>
2024-11-21 10:19:12 +0700

    finishing the home page.

--- front-end/src/ui/Navbar.tsx
+++ front-end/src/ui/Navbar.tsx
@@ -2,22 +2,22 @@ export default function Navbar() {
   return (
     <section className="flex text-white pl-[31.313rem] gap-x-[5.188rem]">
       <a href="#home">
-        <p className="hover:text-lightBlue font-semibold">Home</p>
+        <p className="hover:text-lightBlue active:text-white font-semibold transition ease-in-out duration-300">Home</p>
       </a>
       <a href="#portfolio">
-        <p className="hover:text-lightBlue font-semibold">Portfolio</p>
+        <p className="hover:text-lightBlue active:text-white font-semibold transition ease-in-out duration-300">Portfolio</p>
       </a>
       <a href="#about">
-        <p className="hover:text-lightBlue font-semibold">About</p>
+        <p className="hover:text-lightBlue active:text-white font-semibold transition ease-in-out duration-300">About</p>
       </a>
       <a href="#skill">
-        <p className="hover:text-lightBlue font-semibold">Skill</p>
+        <p className="hover:text-lightBlue active:text-white font-semibold transition ease-in-out duration-300">Skill</p>
       </a>
       <a href="#contact">
-        <p className="hover:text-lightBlue font-semibold">Contact</p>
+        <p className="hover:text-lightBlue active:text-white font-semibold transition ease-in-out duration-300">Contact</p>
       </a>
       <a href="#signin">
-        <p className="hover:text-lightBlue font-semibold">Sign In</p>
+        <p className="hover:text-lightBlue active:text-white font-semibold transition ease-in-out duration-300">Sign In</p>
       </a>
     </section>
   );
