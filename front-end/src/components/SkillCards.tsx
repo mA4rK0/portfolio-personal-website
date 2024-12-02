cb5e2962ff0f21957071129cf262d637fb25e3d1
mA4rK0 <onlymarko25@gmail.com>
2024-12-02 09:45:18 +0700

    refactor the about page for mobile device.

--- front-end/src/components/SkillCards.tsx
+++ front-end/src/components/SkillCards.tsx
@@ -26,14 +26,7 @@ const SkillCards = () => {
   return (
     <section className="flex flex-wrap justify-center gap-x-40 gap-y-14">
       {skills.map((skill: Skill) => (
-        <section
-          key={skill.id}
-          className="rounded-lg shadow-lg hover:shadow-xl hover:shadow-navyBlue shadow-navyBlue transition ease-in-out duration-300 w-[17.125rem] h-[12.938rem]"
-          data-aos="zoom-out-down"
-          data-aos-offset="300"
-          data-aos-duration="1000"
-          data-aos-delay={`${skill.id * 50}`}
-        >
+        <section key={skill.id} className="rounded-lg shadow-lg hover:shadow-xl hover:shadow-navyBlue shadow-navyBlue transition ease-in-out duration-300 w-[17.125rem] h-[12.938rem]">
           <figure className="bg-white h-[9.063rem] flex justify-center rounded-t-lg">
             <Img src={skill.image} alt={skill.alt} width={150} height={150} />
           </figure>
